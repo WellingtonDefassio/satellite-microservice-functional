c00bfaf0960435a3b8b9b9a328d4cb6c117bd293
Wellington Defassio <well.defassio@gmail.com>
2022-06-30 22:53:30 -0300

    commit

---
+++ src/satellite/functions/satellite-transform.ts
@@ -0,0 +1,7 @@
+import { SendMessages } from '@prisma/client';
+
+export const removeUpCreatedAt = (array: SendMessages[]) => {
+  array.filter((message) => {
+    const newArray = [];
+  });
+};
--- src/satellite/functions/satellite-validators-functions.ts
+++
@@ -1,7 +0,0 @@
-import { NotFoundException } from '@nestjs/common';
-
-export const validateNull = (elemento: any, message: string) => {
-  if (!elemento) {
-    throw new NotFoundException(message);
-  } else return elemento;
-};
